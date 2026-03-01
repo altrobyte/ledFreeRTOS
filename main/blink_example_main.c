a15b5b72d05b679130fdb75a80a3708f61b30892
pawameena <pawan@gmial.com>
2026-03-01 17:42:49 +0530

    i changed delay to 800

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -15,14 +15,14 @@ void led_blink_task(void *pvParameters)
     {
         gpio_set_level(LED_GPIO, 1);  // LED ON
         ESP_LOGI(TAG, "LED ON");
-        vTaskDelay(pdMS_TO_TICKS(500));   // 500 ms delay
+        vTaskDelay(pdMS_TO_TICKS(800));   // 500 ms delay
 
         gpio_set_level(LED_GPIO, 0);  // LED OFF
         ESP_LOGI(TAG, "LED OFF");
-        vTaskDelay(pdMS_TO_TICKS(500));   // 500 ms delay
+        vTaskDelay(pdMS_TO_TICKS(800));   // 500 ms delay
     }
 }
-
+// You can add another task here for counting and displaying numbers on the OLED as per your requirement.
 /* ---------------- Main Function ---------------- */
 void app_main(void)
 {
